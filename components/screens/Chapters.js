71ffd0e1d52157c0b0e5563e4f79052ad0733bd4
GeniusPRO271 <benjamintoroj@gmail.com>
2022-11-26 16:02:34 +0700

    update

--- components/screens/Chapters.js
+++ components/screens/Chapters.js
@@ -1,5 +1,7 @@
 import {
+  ActivityIndicator,
   FlatList,
+  SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
@@ -8,8 +10,14 @@ import {
 } from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {useRoute} from '@react-navigation/native';
-import {faAngleLeft, faBars} from '@fortawesome/free-solid-svg-icons';
+import {faAngleLeft, faArrowDown} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {height, width} from '../LibrabyData';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 const Chapters = ({navigation}) => {
   const route = useRoute();
@@ -17,7 +25,22 @@ const Chapters = ({navigation}) => {
   const mangaTitle = route.params?.mangaTitle;
   const [isLoading, SetIsLoading] = useState(true);
   const [chapters, setChapters] = useState();
+  const [isBottom, setIsBottom] = useState(false);
   const tosave = [];
+  const chapter = [];
+  const rotation = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotateZ: withTiming(`${rotation.value}deg`, {
+            duration: 100,
+          }),
+        },
+      ],
+    };
+  });
 
   const GetChapters = async (offset, tosave) => {
     const order = {
@@ -35,7 +58,6 @@ const Chapters = ({navigation}) => {
     }
     const baseUrl = 'https://api.mangadex.org';
     const axios = require('axios');
-    console.log('GetData');
     const resp = await axios({
       method: 'GET',
       url: `${baseUrl}/manga/${mangaId}/feed`,
@@ -46,72 +68,73 @@ const Chapters = ({navigation}) => {
       },
     });
     let total = resp.data.total;
+
     offset = resp.data.offset;
     resp.data.data.map(d => {
-      console.log(d);
+      chapter.push(parseFloat(d.attributes.chapter));
       tosave.push({
         id: d.id,
-        volume: d.attributes.volume,
-        chapter: d.attributes.chapter,
+        chapter: parseFloat(d.attributes.chapter),
         title: d.attributes.title,
       });
     });
     offset < total
       ? GetChapters(offset + 100, tosave)
-      : (setChapters(tosave), SetIsLoading(false));
+      : (chapter.sort((a, b) => b - a),
+        Sort([...new Set(chapter)], tosave),
+        SetIsLoading(false));
+  };
+  const Sort = (chap, tosave) => {
+    arr = [];
+    chap.forEach(element => {
+      tosave.map(d => {
+        d.chapter == element && arr.push(d);
+      });
+    });
+    setChapters(arr);
   };
   useEffect(() => {
     GetChapters(0, tosave);
   }, []);
-
   const Header = () => {
     return (
       <View style={styles.MainHeaderStyle}>
-        <View style={styles.TopBlockSafeAreaView}>
-          <View style={styles.LeftIconStyle}>
-            <TouchableOpacity onPress={() => navigation.goBack()}>
-              <FontAwesomeIcon
-                icon={faAngleLeft}
-                size={20}
-                style={{margin: 15}}
-                color={'#003049'}
-              />
-            </TouchableOpacity>
-          </View>
+        <SafeAreaView
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 50,
+          }}>
           <View
             style={{
+              width: '80%',
+              height: '90%',
               justifyContent: 'center',
               alignItems: 'center',
-              width: 200,
+              paddingHorizontal: 20,
+              backgroundColor: '#F77F00',
+              borderRadius: 30,
             }}>
-            <Text style={{fontSize: 12}}>{mangaTitle}</Text>
+            <Text style={{fontSize: 12, textAlign: 'center', color: 'white'}}>
+              {mangaTitle}
+            </Text>
           </View>
-          <View style={styles.RightIconStyle}>
-            <TouchableOpacity>
-              <FontAwesomeIcon
-                icon={faBars}
-                style={{margin: 15}}
-                size={20}
-                color={'#003049'}
-              />
-            </TouchableOpacity>
-          </View>
-        </View>
+        </SafeAreaView>
       </View>
     );
   };
-  const Content = ({item}) => {
-    let volume = item.volume;
+  const Content = ({item, index, offset}) => {
     let chapter = item.chapter;
     let title = item.title;
     let id = item.id;
+    console.log(offset);
     return (
       <TouchableOpacity
         style={{
           flex: 1,
           backgroundColor: 'white',
           justifyContent: 'center',
-          padding: 20,
+          padding: 15,
         }}
         onPress={() =>
           navigation.navigate('ReadChapter', {
@@ -122,19 +145,55 @@ const Chapters = ({navigation}) => {
           })
         }>
         <Text style={{fontSize: 20}}>
-          Vol:{volume} | Ch:{chapter} | {title}
+          Ch:{chapter} | {title}
         </Text>
       </TouchableOpacity>
     );
   };
-  return (
-    !isLoading && (
-      <View style={{flex: 1}}>
-        <StatusBar hidden />
-        <Header />
-        <FlatList data={chapters} renderItem={Content} />
+  return !isLoading ? (
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <StatusBar hidden />
+      <View>
+        <FlatList
+          data={chapters}
+          renderItem={Content}
+          contentContainerStyle={{paddingTop: height * 0.05}}
+          ref={ref => (listViewRef = ref)}
+        />
       </View>
-    )
+      <Header />
+      <TouchableOpacity
+        onPress={() => {
+          isBottom
+            ? ((rotation.value = 0),
+              listViewRef.scrollToOffset({offset: 0, animated: true}),
+              setIsBottom(false))
+            : ((rotation.value = 180),
+              listViewRef.scrollToEnd({animated: true}),
+              setIsBottom(true));
+        }}
+        style={{
+          bottom: 50,
+          right: width * 0.05,
+          width: 60,
+          height: 60,
+          borderRadius: 60,
+          borderWidth: 1,
+          borderColor: '#F77F00',
+          justifyContent: 'center',
+          alignItems: 'center',
+          position: 'absolute',
+        }}>
+        <Animated.View style={animatedStyle}>
+          <FontAwesomeIcon icon={faArrowDown} color={'#F77F00'} size={30} />
+        </Animated.View>
+      </TouchableOpacity>
+    </SafeAreaView>
+  ) : (
+    <SafeAreaView
+      style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+      <ActivityIndicator size={'large'} />
+    </SafeAreaView>
   );
 };
 
@@ -142,17 +201,17 @@ export default Chapters;
 
 const styles = StyleSheet.create({
   MainHeaderStyle: {
-    height: 100,
+    position: 'absolute',
+    height: 50,
+    top: height * 0.1 - 50,
+    left: 0,
+    right: 0,
     justifyContent: 'center',
-    borderBottomRightRadius: 20,
-    borderBottomLeftRadius: 20,
-    backgroundColor: '#F77F00',
     alignItems: 'center',
-    shadowColor: 'black',
-    shadowOpacity: 0.5,
   },
   TopBlockSafeAreaView: {
     flexDirection: 'row',
+    justifyContent: 'center',
     marginTop: 20,
   },
   LeftIconStyle: {
